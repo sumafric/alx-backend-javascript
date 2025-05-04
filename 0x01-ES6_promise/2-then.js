d5c488d3ce9b7cfe0622be60bad9ae7c09e9a320
Sumafric <abelsum1330@gmail.com>
2025-05-04 19:46:50 +0000

    Dont make a promise you cant keep

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+/**
+ * the function handles a promise response
+ * .then() handles a successful response
+ *  .catch() handles a failed response
+ *  .finally() handles any response regardless of success or failure
+ */
+export default function handleResponseFromAPI(promise) {
+  const object = { status: 200, body: 'success' };
+  return promise
+    .then(() => object)
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
